e70831e5b4edbb8c9e822cd3e2ee3906403e0310
Osmário Júnior <osmariojj@gmail.com>
2022-07-22 14:48:23 -0300

    refactor: Refactoring http request and create unit test

--- src/infra/swapi_api_consumer_test.py
+++ src/infra/swapi_api_consumer_test.py
@@ -1,11 +1,17 @@
 from .swapi_api_consumer import SwapiApiConsumer
 
-def test_get_starships(requests_mock):
+def test_get_starships():
     '''Testing get_starships method'''
 
-    requests_mock.get('https://swapi.dev/api/starships/', status_code=200, json={'some': 'thing'})
+    #requests_mock.get('https://swapi.dev/api/starships/', status_code=200, json={'some': 'thing'})
 
     swapi_api_consumer = SwapiApiConsumer()
-    response = swapi_api_consumer.get_starships(page=1)
+    page = 1
+    get_starships_response = swapi_api_consumer.get_starships(page)
 
-    print(response)
+
+    assert get_starships_response.request.method == 'GET'
+    assert get_starships_response.request.url == 'https://swapi.dev/api/starships/'
+    assert get_starships_response.request.params == {"page": page}
+    assert get_starships_response.status_code == 200
+    assert isinstance(get_starships_response.response["results"], list)
