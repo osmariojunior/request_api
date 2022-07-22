41941c508eed6c6950ebc52064679f7bf075dd5b
Osmário Júnior <osmariojj@gmail.com>
2022-07-22 19:24:59 -0300

    feat: Finishing starships list colector

--- src/data/usecases/starships_list_colector.py
+++ src/data/usecases/starships_list_colector.py
@@ -1,7 +1,6 @@
 from typing import Dict, List, Type
 from src.data.interfaces.swapi_api_consumer import SwapiApiConsumerInterface
 from src.domain.usecases import StarshipsListColectorInteface
-from src.infra.swapi_api_consumer import SwapiApiConsumer
 
 class StarshipsListColector(StarshipsListColectorInteface):
     ''' Starships colector usecase'''
@@ -11,5 +10,23 @@ class StarshipsListColector(StarshipsListColectorInteface):
 
 
     def list(self, page: int) -> List[Dict]:
-        response = self.__api_consumer.get_starships(page)
-        print(response)
+        api_response = self.__api_consumer.get_starships(page)
+        starships_formated_list = self.__format_api_response(api_response.response["results"])
+        return starships_formated_list
+
+
+    @classmethod
+    def __format_api_response(cls, results: List[Dict]):
+        starships_formated_list = []
+
+        for starship in results:
+            starships_formated_list.append({
+                "id": starship["url"].split("/")[-2],
+                "name": starship["name"],
+                "model": starship["model"],
+                "max_atmosphering_speed": starship["max_atmosphering_speed"],
+                "hyperdrive_rating": starship["hyperdrive_rating"],
+                "MGLT": starship["MGLT"]
+            })
+        
+        return starships_formated_list
--- src/data/usecases/starships_list_colector_test.py
+++ src/data/usecases/starships_list_colector_test.py
@@ -1,11 +1,18 @@
 from src.domain.usecases import starships_list_colector
 from .starships_list_colector import StarshipsListColector
-from src.infra.swapi_api_consumer import SwapiApiConsumer
+from src.infra.test.swapi_api_consumer import SwapiApiConsumerSpy
 
 def test_list():
-    api_consumer = SwapiApiConsumer()
+    '''test list method'''
+    api_consumer = SwapiApiConsumerSpy()
     starships_list_colector = StarshipsListColector(api_consumer)
 
     page = 1
-    starships_list_colector.list(page)
+    response = starships_list_colector.list(page)
+
+    assert api_consumer.get_starships_attributes == {"page": page}
+    assert isinstance(response, list)
+    assert "id" in response[0]
+    assert "MGLT" in response[0]    
+
     
