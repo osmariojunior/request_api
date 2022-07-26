6cef28b444b6640b303f355fe319cb78560cae50
Osmário Júnior <osmariojj@gmail.com>
2022-07-26 18:47:55 -0300

    feat: Implementhing validators witn cerberus

--- src/main/routes/starships_router.py
+++ src/main/routes/starships_router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Request
+from src.validators.get_starships_in_pagination_validator import get_pagination_validator
 
 
 starships_router = APIRouter()
@@ -6,4 +7,8 @@ starships_router = APIRouter()
 
 @starships_router.get('/api/starships/list')
 def get_starships_in_pagination(request: Request):
+
+
+    get_pagination_validator(request)
+
     return {"msg": "ola"}
