aa434c48019c08dc72c623a0f6022890bfccb3f3
Osmário Júnior <osmariojj@gmail.com>
2022-07-26 22:19:00 -0300

    feat: Implementing get_starship_information

--- src/infra/swapi_api_consumer.py
+++ src/infra/swapi_api_consumer.py
@@ -11,6 +11,7 @@ class SwapiApiConsumer(SwapiApiConsumerInterface):
 
     def __init__(self) -> None:
         self.get_starships_response = namedtuple('GET_Starships', 'status_code request response')
+        self.get_starships_information_response = namedtuple('GET_Starships_Info', 'status_code request response')
 
 
     def get_starships(self, page: int) -> Tuple[int, Type[Request], Dict]:
@@ -28,6 +29,27 @@ class SwapiApiConsumer(SwapiApiConsumerInterface):
 
         if (status_code >=200) and (status_code <=299):
             return self.get_starships_response(
+                status_code=status_code, request=req, response=response.json()
+            )
+        else:
+            raise HttpRequestError(
+                message=response.json()["detail"], status_code=status_code
+            )
+
+    def get_starships_information(self, starship_id: int) -> Tuple[int, Type[Request], Dict]:
+        '''request starships pagination'''
+
+        req = requests.Request(
+            method='GET',
+            url=f'https://swapi.dev/api/starships/{starship_id}/',
+        )
+        req_prepared = req.prepare()
+
+        response = self.__send_http_request(req_prepared)
+        status_code = response.status_code
+
+        if (status_code >=200) and (status_code <=299):
+            return self.get_starships_information_response(
                 status_code=status_code, request=req, response=response.json()
             )
         else:
--- src/infra/swapi_api_consumer_test.py
+++ src/infra/swapi_api_consumer_test.py
@@ -1,3 +1,4 @@
+import requests_mock
 from src.errors.http_request_error import HttpRequestError
 from .swapi_api_consumer import SwapiApiConsumer
 
@@ -18,6 +19,7 @@ def test_get_starships(requests_mock):
     assert isinstance(get_starships_response.response["results"], list)
 
 def test_get_starships_http_error(requests_mock):
+    '''Testing get_starships method error'''
 
     requests_mock.get('https://swapi.dev/api/starships/', status_code=404, json={'detail': 'Not found'})
 
@@ -25,6 +27,49 @@ def test_get_starships_http_error(requests_mock):
     page = 100
     try:
         swapi_api_consumer.get_starships(page=page)
+        assert True is False
+    except HttpRequestError as error:
+        assert error.message is not None
+        assert error.status_code is not None
+        print(error)
+
+
+def test_get_starships_information(requests_mock):
+    '''Testing test_get_starships_information method'''
+
+    starship_id = 9
+    swapi_api_consumer = SwapiApiConsumer()
+
+    requests_mock.get(
+        f'https://swapi.dev/api/starships/{starship_id}/', 
+        status_code=200, 
+        json={'name': 'some', 'model': 'thing', 'MGLT': '123'}
+    )
+
+    starship_information = swapi_api_consumer.get_starships_information(starship_id)
+
+
+    assert starship_information.request.method == 'GET'
+    assert starship_information.request.url == f'https://swapi.dev/api/starships/{starship_id}/'
+    assert starship_information.status_code == 200
+
+    assert "MGLT" in starship_information.response
+
+    
+def test_get_starships_information_error(requests_mock):
+    '''Testing test_get_starships_information method error'''
+
+    starship_id = 1
+    swapi_api_consumer = SwapiApiConsumer()
+
+    requests_mock.get(
+        f'https://swapi.dev/api/starships/{starship_id}/', 
+        status_code=404, 
+        json={'detail': 'something'}
+    )
+
+    try:
+        swapi_api_consumer.get_starships_information(starship_id)
         assert True is False
     except HttpRequestError as error:
         assert error.message is not None
